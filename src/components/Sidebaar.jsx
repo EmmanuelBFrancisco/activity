9e781b5c5ad87df1a61df924659add732af64f18
EmmanuelBFrancisco <159740277+EmmanuelBFrancisco@users.noreply.github.com>
2024-02-19 14:22:26 +0800

    Add files via upload

---
+++ src/components/Sidebaar.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+
+function Sidebaar (){
+    return (
+            <div className='main-right'>
+            <h1>Sideright</h1>
+            </div>
+    
+
+    )
+}
+export default Sidebaar
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+
+function Sidebar (){
+    return (
+        <div className='main-content'>
+            <h1>Sideleft</h1>
+        </div>
+
+    )
+}
+export default Sidebar
